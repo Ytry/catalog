a4453f611985d264a01bccb989dda1b16cb744f2
Ytry <Kyle12qw@gmail.com>
2015-10-27 06:19:31 -0400

    Item table
    Added item table to contain data about items in the catalog

--- database_setup.py
+++ database_setup.py
@@ -41,6 +41,16 @@ class Category(Base):
         }
 
 
+class Item(Base):
+    """Create table to hold Item data"""
+    __tablename__ = 'item'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    description = Column(String)
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+
 
 
 engine = create_engine('sqlite:///catalog.db')
