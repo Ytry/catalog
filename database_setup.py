02fee90f9baa6c2f46200fd7b8451dd43692b841
Ytry <Kyle12qw@gmail.com>
2015-10-27 06:12:49 -0400

    User table
    Added User table to hold user data

--- database_setup.py
+++ database_setup.py
@@ -7,6 +7,14 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 
 
+class User(Base):
+    """Create table to hold User data"""
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False)
+    email = column(String(250))
+
 
 
 engine = create_engine('sqlite:///catalog.db')
