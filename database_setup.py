02985f3811bd5b8402a14a9e9cece67d39f6f3ee
Ytry <Kyle12qw@gmail.com>
2015-10-27 06:11:58 -0400

    initial
    Imported dependencies and set up SQL Alchemy

---
+++ database_setup.py
@@ -0,0 +1,15 @@
+# import SQL Alchemy dependencies
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+
+
+engine = create_engine('sqlite:///catalog.db')
+
+
+Base.metadata.create_all(engine)
