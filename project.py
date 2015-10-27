b5b0254ebfcdb35de835be7c41af626f6cda63e6
Ytry <Kyle12qw@gmail.com>
2015-10-27 11:10:04 -0400

    added showItem function
    Added showItem function as well as a route to it's template even though
    it isn't created yet

--- project.py
+++ project.py
@@ -11,6 +11,13 @@ def showCategories():
 
     return render_template('categories.html')
 
+
+@app.route('/item/<int:category_id>/<int:item_id>')
+def showItem(category_id, item_id):
+
+    return render_template('item.html')
+
+
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
