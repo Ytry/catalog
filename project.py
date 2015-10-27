8325eb6c67077ce6cb5c406f8e8f679c44c0d40d
Ytry <Kyle12qw@gmail.com>
2015-10-27 06:40:09 -0400

    route to homepage
    Added route to homepage and return a flask template when you land there.
    Imported render_template flask dependency as well

--- project.py
+++ project.py
@@ -1,7 +1,15 @@
-from flask import flask
+from flask import flask, render_template
 
 app = Flask(__name__)
 
+
+# route to website homepage / category page
+@app.route('/')
+@app.route('/category')
+@app.route('/categories')
+def showCategories():
+
+    return render_template('categories.html')
 
 if __name__ == 'main':
     app.debug = True
